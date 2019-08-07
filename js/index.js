2c7bfeb33fa0256728d2067743c92ed98b6c5e52
RichardEsquivel <rlesquivel@gmail.com>
2019-08-06 19:23:41 -0500

    Created mouseover and mouseleave event listener to the logo-heading to have it increase in size, change color and font when mouse over and go back to original stylings once mouse leaves. I also used Greensock to have the image for fun-bus in the header colorize and fade to gray when mouse moved over and out. Added an event listener on mouse out to have .text-content flicker in and out and eventually disappear. Buttons for signup at bottom also wiggle up on the y axis when clicked and when mouse leaves they go back to normal position.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,80 @@
-// Your code goes here
+//****Nav Bar***
+
+// Logo-Heading animation to increase font size with mouseover event listener and then revert
+//back to normal state with mouse leave
+const buttonFunBus = document.querySelector('.logo-heading');
+
+buttonFunBus.addEventListener('mouseover', (event) => {
+	buttonFunBus.style.color = 'red';
+	buttonFunBus.style.fontSize = '7.5rem';
+	buttonFunBus.style.fontFamily = 'Candara';
+	buttonFunBus.style.transitionDuration = '.1s';
+});
+
+buttonFunBus.addEventListener('mouseleave', (event) => {
+	buttonFunBus.style.color = 'black';
+	buttonFunBus.style.fontSize = '3rem';
+	buttonFunBus.style.fontFamily = 'Indie Flower';
+	buttonFunBus.style.transitionDuration = '.5s';
+});
+
+//****Header****
+
+// Create funBusHeader to have blur animation appear and disapper on fun bus in the sand image on click and dbl click with
+//event listener and utilizing Greensock
+const funBusHeader = document.querySelector('.funBusHeader');
+funBusHeader.addEventListener('mouseenter', (e) => {
+	TweenMax.to(e.target, 1, {
+		scale: 0.8,
+		filter: 'none',
+		ease: Elastic.easeOut.config(1, 0.55)
+	});
+});
+funBusHeader.addEventListener('mouseout', (e) => {
+	TweenMax.to(e.target, 0.5, {
+		scale: 1,
+		filter: 'grayscale(1) blur(.01px)',
+		ease: Power1.easeIn
+	});
+});
+
+//***Body***
+
+const scaryText = document.querySelector('.text-content');
+
+scaryText.addEventListener('mouseleave', (e) => {
+	TweenMax.to(e.target, 2, {
+		opacity: 0,
+		ease: RoughEase.ease.config({ strength: 3, points: 20, randomize: true }),
+		repeat: -1,
+		yoyo: true
+	});
+});
+
+//Sign Me Up Buttons Near Footer to Choose Destinations
+const signMeUpBtn = document.querySelectorAll('.content-pick .btn');
+
+signMeUpBtn.forEach((SignUp) => {
+	SignUp.addEventListener('click', (e) => {
+		TweenMax.to(e.target, 0.5, {
+			ease: SteppedEase.config(20),
+			y: -10
+		});
+	});
+});
+
+signMeUpBtn.forEach((SignUp) => {
+	SignUp.addEventListener('mouseout', (e) => {
+		TweenMax.to(e.target, 2.5, {
+			ease: RoughEase.ease.config({
+				template: Power0.easeNone,
+				strength: 1,
+				points: 20,
+				taper: 'none',
+				randomize: true,
+				clamp: false
+			}),
+			y: 10
+		});
+	});
+});
